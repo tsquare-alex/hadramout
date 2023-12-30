dc90a2c634328a18762e5c53a0696639975ffa70
Ahmed Shahawy <ahmed.shahawy@yahoo.com>
2023-12-30 20:37:22 +0200

    fake push

--- lib/features/dashboard/presentation/screens/dashboard_screen.dart
+++ lib/features/dashboard/presentation/screens/dashboard_screen.dart
@@ -7,7 +7,7 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Dashboard Screen'),
+        child: Text('Shahawy Dashboard Screen'),
       ),
     );
   }
