06a55359788ffcfcf9f7c3577c972748790ee7e8
montasermohamed101 <montasermohamed101@gmail.com>
2024-01-01 13:29:05 +0200

    methods of logic has been created for dashboard

--- lib/features/dashboard/cubit/dashboard_cubit.dart
+++ lib/features/dashboard/cubit/dashboard_cubit.dart
@@ -4,12 +4,14 @@ import 'package:hadrmouthamza/core/common/models/section.dart';
 import 'package:hadrmouthamza/core/common/models/species.dart';
 import 'package:hadrmouthamza/src/app_export.dart';
 
+import '../../../core/common/models/dishes.dart';
 import '../data/repository/dashboard_repository.dart';
 
 part 'dashboard_state.dart';
 
 class DashboardBloc extends Cubit<DashboardState> {
   final DashboardRepository _dashboardRepository;
+
   DashboardBloc(
     this._dashboardRepository,
   ) : super(DashboardInitial());
@@ -17,15 +19,37 @@ class DashboardBloc extends Cubit<DashboardState> {
   static DashboardBloc get(context) => BlocProvider.of<DashboardBloc>(context);
 
   late List<OrderModel> _orders = [];
+
   List<OrderModel> get orders => _orders;
+
+  late List<DishesModel> _dishes = [];
+
+  List<DishesModel> get dishes => _dishes;
+
+  late List<SectionModel> _sections = [];
+
+  List<SectionModel> get sections => _sections;
+
+  late List<SpeciesModel> _species = [];
+
+  List<SpeciesModel> get species => _species;
   late String errorMessage;
 
+  //todo Order's Method
   Future<void> getOrders() async {
+    emit(GetOrderDashboardLoading());
     final orders = await _dashboardRepository.getOrders();
-    orders.isEmpty ? errorMessage = 'No data' : _orders = orders;
+    // orders.isEmpty ? errorMessage = 'No data' : _orders = orders;
+    if (orders.isEmpty) {
+      emit(GetOrderDashboardError(errorMessage: 'No data'));
+    } else {
+      _orders = orders;
+      emit(GetOrderDashboardSuccess());
+    }
   }
 
   Future<void> addOrder() async {
+    emit(AddOrderDashboardLoading());
     var order = OrderModel(
       id: '',
       cancelled: false,
@@ -54,7 +78,86 @@ class DashboardBloc extends Cubit<DashboardState> {
       ],
       dishes: null,
     );
-
     await _dashboardRepository.addOrder(order);
+    emit(AddOrderDashboardSuccess());
+  }
+
+  //Todo Dishes's Method
+
+  Future<void> getDishes() async {
+    emit(GetDishDashboardLoading());
+    final dishes = await _dashboardRepository.getDishes();
+    // dishes.isEmpty ? errorMessage = 'No data' : _dishes = dishes;
+    if (dishes.isEmpty) {
+      emit(GetDishDashboardError(errorMessage: "'No data'"));
+    } else {
+      emit(GetDishDashboardSuccess());
+    }
+  }
+
+  Future<void> addDish() async {
+    emit(AddDishDashboardLoading());
+    var dish = DishesModel(
+      id: "",
+      title: "Welcome Dish",
+      description: "Montaser & Shahawy Created amazing Dish",
+      image:
+          "https://img.freepik.com/free-photo/beautiful-sea-ocean-with-cloud-blue-sky_74190-6828.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1704067200&semt=ais",
+      price: 90000000000,
+      createdAt: DateTime.now().toString(),
+      section: SectionModel(id: "id", title: "Chicken"),
+      offer: false,
+      offerValue: 20,
+    );
+    await _dashboardRepository.addDish(dish);
+    emit(AddDishDashboardSuccess());
+  }
+
+  //Todo Dishes's Method
+
+  Future<void> getSections() async {
+    emit(GetSectionDashboardLoading());
+    final sections = await _dashboardRepository.getSections();
+    // dishes.isEmpty ? errorMessage = 'No data' : _dishes = dishes;
+    if (sections.isEmpty) {
+      emit(GetSectionDashboardError(errorMessage: "'No data'"));
+    } else {
+      emit(GetSectionDashboardSuccess());
+    }
+  }
+
+  Future<void> addSection() async {
+    emit(AddSectionDashboardLoading());
+    var section = SectionModel(id: "id", title: "Section");
+    await _dashboardRepository.addSection(section);
+    emit(AddSectionDashboardSuccess());
+  }
+
+  //Todo Species Method
+
+  Future<void> getSpecies() async {
+    emit(GetSpeciesDashboardLoading());
+    final species = await _dashboardRepository.getSpecies();
+    // dishes.isEmpty ? errorMessage = 'No data' : _dishes = dishes;
+    if (species.isEmpty) {
+      emit(GetSpeciesDashboardError(errorMessage: "'No data'"));
+    } else {
+      emit(GetSpeciesDashboardSuccess());
+    }
+  }
+
+  Future<void> addSpecies() async {
+    emit(AddSpeciesDashboardLoading());
+    var section = SpeciesModel(
+        id: "id",
+        title: "New Meals",
+        price: 30099,
+        createdAt: DateTime.now().toIso8601String(),
+        section: SectionModel(
+          id: "",
+          title: "New Meals"
+        ));
+    await _dashboardRepository.addSpecies(section);
+    emit(AddSpeciesDashboardSuccess());
   }
 }
--- lib/features/dashboard/data/data_source/dashboard_data_source.dart
+++ lib/features/dashboard/data/data_source/dashboard_data_source.dart
@@ -1,4 +1,7 @@
+import 'package:hadrmouthamza/core/common/models/dishes.dart';
 import 'package:hadrmouthamza/core/common/models/order.dart';
+import 'package:hadrmouthamza/core/common/models/section.dart';
+import 'package:hadrmouthamza/core/common/models/species.dart';
 import 'package:hadrmouthamza/src/app_export.dart';
 
 class DashboardDataSource {
@@ -12,4 +15,34 @@ class DashboardDataSource {
     final data = await _firebaseDatabase.collection('orders').add(order.toJson());
     await _firebaseDatabase.collection('orders').doc(data.id).update({'id': data.id});
   }
+
+  Future<QuerySnapshot<Map<String, dynamic>>> getDishes() async {
+    return await _firebaseDatabase.collection('dishes').get();
+  }
+
+  Future<void> addDish(DishesModel dish) async {
+    final data = await _firebaseDatabase.collection('dishes').add(dish.toJson());
+    await _firebaseDatabase.collection('dishes').doc(data.id).update({'id': data.id});
+  }
+
+
+  Future<QuerySnapshot<Map<String, dynamic>>> getSections() async {
+    return await _firebaseDatabase.collection('sections').get();
+  }
+
+  Future<void> addSection(SectionModel section) async {
+    final data = await _firebaseDatabase.collection('sections').add(section.toJson());
+    await _firebaseDatabase.collection('sections').doc(data.id).update({'id': data.id});
+  }
+
+  Future<QuerySnapshot<Map<String, dynamic>>> getSpecies() async {
+    return await _firebaseDatabase.collection('species').get();
+  }
+
+  Future<void> addSpecies(SpeciesModel species) async {
+    final data = await _firebaseDatabase.collection('species').add(species.toJson());
+    await _firebaseDatabase.collection('species').doc(data.id).update({'id': data.id});
+  }
+
+
 }
