25afb39353871ac78dad8d7f520a79e2838c01a7
ahmed <ao1922302@gmail.com>
2024-01-04 16:57:01 -0800

    change padding value

--- lib/features/home/presentation/widgets/casserole_section.dart
+++ lib/features/home/presentation/widgets/casserole_section.dart
@@ -7,7 +7,7 @@ class CasseroleSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 60),
+      padding: const EdgeInsets.symmetric(horizontal: 61),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
